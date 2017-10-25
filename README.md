c96980f43bc2312c92251fd86a3774981b8e5da3
itgsod-Herman-Gradin <herman.gradin@elev.it-gymnasiet.se>
2017-10-25 13:15:16 +0200

    klar

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 
 Simple program that calculates fares.
 
+##  Jag ska göra ett program som bestämer vilket pris olika åldersgrupper ska få på sl biljetter
+
+### Jag ska göra detta genom ett program som ser vilken
+
--- sl.se.py
+++ sl.se.py
@@ -1,11 +1,22 @@
-def fares(age, student=False, senior=False):
-    print age,student,senior
-    return 'hel'
+def fares(age,child=False, student=False, senior=False):
+   if child or student or senior:
+       return 'Halv'
+   else:
+       if age < 18 or age > 65:
+           return 'Halv'
+       else:
+           return 'Hel'
 
 
 
 
 
-fares(10)
-fares(65,senior=True)
-fares(25,student=True)
+
+
+
+
+print 'barn 10' ,fares(10)
+print 'pensionar 65' ,fares(66)
+print 'sjukpensionar' ,fares(47,senior=True)
+print 'student 25' ,fares(25,student=True)
+print 'vuxen 45' ,fares(45)
